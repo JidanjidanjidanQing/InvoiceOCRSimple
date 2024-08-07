2c30dc54f58475c504d059ba513757d9197771c2
pusher <jidanjidanjidanqing@outlook.com>
2024-08-07 10:55:46 +0800

    修改PaddleOCR接口，用于对接RPA

---
+++ Controllers/PaddleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OCRSimple.Controllers
+{
+    public class PaddleController
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public class ReadPDFController : ControllerBase
+        {
+
+            private readonly ILogger<ReadPDFController> _logger;
+
+            public ReadPDFController(ILogger<ReadPDFController> logger)
+            {
+                _logger = logger;
+            }
+
+
+            //[HttpPost(Name = "PostReadPDF")]
+            //public Task<IActionResult> PaddleOCR(string filepath, string filetype)
+            //{
+            //    ret;
+
+            //}
+        }
+    }
+}
